c7aba861e95aeae4b138145cee440c7a10254132
Fábio Vicente <fab10_lima@hotmail.com>
2022-03-29 19:18:52 -0300

    Requisito 2, 3 e 4: Finalizado - implementando os campos da tela de Login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Input from '../Components/Input';
 
 function Login() {
-  console.log('Eita');
   return (
-    <h1>Eu sou o Login</h1>
+    <form>
+      <Input name="email" type="email" testId="email-input" />
+      <Input name="senha" type="password" testId="password-input" />
+      <button type="submit" data-testid="login-submit-btn">Enter</button>
+    </form>
   );
 }
+
 export default Login;
