a8b52b33766727197513445ca0b2abbf06141081
Fábio Vicente <fab10_lima@hotmail.com>
2022-04-05 09:29:33 -0300

    Requisito 82: concluído - inserindo os elementos da página `Login`

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -4,10 +4,29 @@ import Header from '../Components/Header';
 
 function Profile() {
   return (
-    <div>
+    <>
       <Header title="Profile" />
+      <p data-testid="profile-email">Email</p>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Done Recipes
+      </button>
       <Footer />
-    </div>
+    </>
   );
 }
 export default Profile;
